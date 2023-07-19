55ae34d11b903f479e3b99e5fd3a43c70dd24268
Maciej Skorus <maciej.skorus@anfata.com>
2023-07-19 21:06:18 +0200

    fix: build error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
+        primary: ['Inter', ...defaultTheme.fontFamily.sans],
         mont: ['Montserrat'],
       },
       colors: {
