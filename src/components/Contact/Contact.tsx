75ad27924cd1962dbd93e38f536d024bd0d6be94
Maciej Skorus <maciej.skorus@anfata.com>
2023-07-20 22:14:23 +0200

    feat: mobile scene test

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -8,12 +8,12 @@ export default function Contact(): JSX.Element {
       className='font-mont ny-20 relative flex h-auto w-full flex-col items-center justify-center overflow-hidden bg-white'
     >
       <div className='arrow-down blue' />
-      <h3 className='font-mont text-primary-blue -left-6 top-[150px] mt-[60px] text-xl font-[500] leading-3 tracking-[10px] md:absolute md:mt-0 md:-rotate-90'>
+      <h3 className='font-mont text-primary-blue -left-6 top-[170px] mb-6 mt-[60px] text-xl font-[500] leading-3 tracking-[10px] md:absolute md:mb-0 md:mt-0 md:-rotate-90'>
         CONTACT
       </h3>
       <div className='gradient-slow border-primary-blue xsm:w-[450px] my-20 mt-10 flex h-full w-full flex-col items-center gap-3 border-t-2 text-white md:my-20 md:flex-col'>
-        <IoMailOpenSharp className='text-primary-blue my-2 text-center text-8xl' />
-        <form className='w-full px-6'>
+        <IoMailOpenSharp className='text-primary-blue xsm:mt-4 mt-5 text-center text-8xl duration-150 hover:cursor-pointer hover:text-white' />
+        <form className='xsm:px-6 w-full px-5'>
           <div className='xsm:flex-row xsm:gap-2 mb-3 flex w-full flex-col justify-between gap-5'>
             <div className='xsm:min-w-[180px] relative h-11 w-full'>
               <input
@@ -49,13 +49,13 @@ export default function Contact(): JSX.Element {
                 className='font-mont peer h-[250px] w-full resize-none border-none  border-transparent bg-transparent px-2 pb-1.5 pt-4 text-sm font-[500] text-white outline-none transition-all focus:border-none focus:border-transparent focus:shadow-none focus:outline-none focus:ring-0 focus:drop-shadow-none disabled:border-0'
                 placeholder=' '
               />
-              <div className='bg-primary-blue xsm:w-[404px] -mt-1 h-[1px] w-full' />
+              <div className='bg-primary-blue -mt-2 h-[1px] w-full' />
               <label className="after:content[' '] text-primary-blue pointer-events-none absolute -top-1.5 left-0 flex h-full w-full select-none text-[11px] font-normal leading-tight transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-white after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-white peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-white peer-focus:after:scale-x-100 peer-focus:after:border-white peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-white">
                 Message
               </label>
             </div>
           </div>
-          <button className='bg-primary-blue hover:text-raspberry mb-5 mt-2 w-full py-3 text-xl tracking-[6px] text-white duration-150 hover:bg-white hover:font-[500] hover:tracking-[10px]'>
+          <button className='bg-primary-blue hover:text-raspberry xsm:mb-5 mb-6 mt-2 w-full py-3 text-xl tracking-[6px] text-white duration-150 hover:bg-white hover:font-[500] hover:tracking-[10px]'>
             SEND
           </button>
         </form>
