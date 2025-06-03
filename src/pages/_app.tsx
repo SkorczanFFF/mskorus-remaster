a052983e7f143e372cc16c113598eca5c4986e43
SkorczanFFF <skorek11@gmail.com>
2025-06-03 22:49:11 +0200

    feat: lenis for smooth scrolling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
+import Lenis from 'lenis';
 import { AppProps } from 'next/app';
+import { useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
@@ -7,6 +9,31 @@ import '@/styles/globals.css';
 import Header from '@/components/layout/Header/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      syncTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <>
       <ToastContainer
